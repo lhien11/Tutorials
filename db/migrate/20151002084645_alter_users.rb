6fb30be0a1766d5f7299953c4835ba0860514e47
Hien Le <lhien11@gmail.com>
2015-10-06 23:44:53 -0500

    first commit

--- db/migrate/20151002084645_alter_users.rb
+++ db/migrate/20151002084645_alter_users.rb
@@ -1,8 +1,8 @@
 class AlterUsers < ActiveRecord::Migration
   def up
-  	# rename_table("users", "admin_users")
-  	# add_column("admin_users", "username", :string, :limit => 25, :after => "email")
-  	# change_column("admin_users", "email", :string, :limit => 100)
+  	rename_table("users", "admin_users")
+  	add_column("admin_users", "username", :string, :limit => 25, :after => "email")
+  	change_column("admin_users", "email", :string, :limit => 100)
   	rename_column("admin_users", "password", "hashed_password")
   	puts "*** Adding an index is next ***"
   	add_index("admin_users", "username")
